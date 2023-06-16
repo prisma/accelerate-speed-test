822bf5226513304036113136d12ca1d5ad9716e3
Ankur Datta <ankur@Ankurs-MacBook-Pro.local>
2023-06-16 14:32:18 +0600

    feat: add queries with cache info

--- lib/queries.ts
+++ lib/queries.ts
@@ -14,3 +14,29 @@ export async function fetchSomeData(cache: boolean): Promise<void> {
     },
   });
 }
+
+interface FetchOptions {
+  runHeavyQuery: boolean;
+  cacheQuery: boolean;
+}
+
+export async function fecthData({ runHeavyQuery, cacheQuery }: FetchOptions) {
+  const res = await prisma.linkOpen
+    .count({
+      cacheStrategy: cacheQuery ? { ttl: 60 * 60 * 24 } : undefined,
+      take: runHeavyQuery ? undefined : 1,
+      where: {
+        link: {
+          User: {
+            email: {
+              contains: ".net",
+            },
+          },
+        },
+      },
+    })
+    .withAccelerateInfo();
+
+  const cacheStatus = res.info?.cacheStatus;
+  return cacheStatus == "ttl" || cacheStatus == "swr" ? 1 : 0;
+}
