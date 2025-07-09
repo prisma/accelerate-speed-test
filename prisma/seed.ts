94043b045fb54539e9b7ee2c7ce620ed83f31b3e
Tyler Benfield <benfield@prisma.io>
2025-07-09 18:23:58 -0400

    update to PPg database (#16)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,34 @@
-import { prisma } from "../lib/prisma";
 import { faker } from "@faker-js/faker";
+import { prisma } from "../lib/prisma";
 
 const main = async () => {
-  //   for (let index = 0; index < 500_000; index++) {
-  //     await prisma.user.create({
-  //       data: {
-  //         email: faker.internet.exampleEmail(),
-  //         name: faker.internet.displayName(),
-  //         links: {
-  //           create: {
-  //             url: faker.internet.url(),
-  //             shortUrl: faker.internet.url(),
-  //             LinkOpen: {
-  //               create: {},
-  //             },
-  //           },
-  //         },
-  //       },
-  //     });
-  //   }
+  const batches = 500;
+  const batchSize = 1_000;
+  for (let index = 0; index < batches; index++) {
+    console.info(`batch ${index + 1}/${batches}`);
+    const users = await prisma.user.createManyAndReturn({
+      data: Array.from({ length: batchSize }, () => {
+        return {
+          email: faker.internet.exampleEmail(),
+          name: faker.internet.displayName(),
+        };
+      }),
+    });
+    const links = await prisma.link.createManyAndReturn({
+      data: users.map((user) => {
+        return {
+          url: faker.internet.url(),
+          shortUrl: faker.internet.url(),
+          userId: user.id,
+        };
+      }),
+    });
+    await prisma.linkOpen.createMany({
+      data: links.map((link) => {
+        return { linkId: link.id };
+      }),
+    });
+  }
 };
 
 main().then(() => console.log("Done seeding"));
