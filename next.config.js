99265b620d367a4754ef3c0d17a352cea80241dd
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-08-01 20:18:44 +0600

    fix: add csp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // This applies the header to all routes in your app.
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://kit.fontawesome.com; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "font-src 'self' data: https://kit.fontawesome.com; " +
+              "connect-src 'self' https://vitals.vercel-insights.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
