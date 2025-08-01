3145c9fb00cab71e7e11dae3c6e8fd2bfa9b4675
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-08-01 20:34:37 +0600

    fix: add csp headers (#21)
    * fix: add cors origin all
    
    * fix: add csp
    
    * fix: add all perms for csp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src * 'unsafe-inline' 'unsafe-eval'; " +
+              "script-src * 'unsafe-inline' 'unsafe-eval'; " +
+              "connect-src * 'unsafe-inline'; " +
+              "img-src * data: blob: 'unsafe-inline'; " +
+              "frame-src *; " +
+              "style-src * 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
