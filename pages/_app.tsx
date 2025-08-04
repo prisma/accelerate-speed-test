99481978505cdd6750a0ffd7f5cb55706d6b0aa0
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-08-04 18:56:12 +0600

    feat: deploy to cf (#22)
    * feat: deploy to cf
    
    * fix: add proper icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import "../styles/index.scss";
 import type { AppProps } from "next/app";
 import { LensProvider } from "@prisma/lens/dist/web";
 import Head from "next/head";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LensProvider>
+      <Script src="https://kit.fontawesome.com/e87031b682.js" crossOrigin="anonymous" async></Script>
       <Head>
         <link rel="icon" href="/favicon.png" />
-        <script src="https://kit.fontawesome.com/e87031b682.js" crossOrigin="anonymous" async></script>
       </Head>
       <Component {...pageProps} />
     </LensProvider>
